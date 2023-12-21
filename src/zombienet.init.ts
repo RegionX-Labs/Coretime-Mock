972a712e86b05aab8f1d20f208f7b1c5a0f8f7cf
Szegoo <sakacszergej@gmail.com>
2023-12-21 17:18:07 +0100

    setting config works

--- src/zombienet.init.ts
+++ src/zombienet.init.ts
@@ -1,4 +1,5 @@
 import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import { u8aToHex } from '@polkadot/util';
 import { KeyringPair } from "@polkadot/keyring/types";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { Timeslice } from "./types";
@@ -21,14 +22,55 @@ async function init() {
 
   await cryptoWaitReady();
 
-  if(featureFlag("fullNetwork")) {
+  if(featureFlag(FULL_NETWORK)) {
     await openHrmpChannel(rococoApi, CORETIME_CHAIN_PARA_ID, CONTRACTS_CHAIN_PARA_ID);
   }
 
-  //await startBulkSale(rococoApi, coretimeApi);
+  await configureBroker(rococoApi, coretimeApi);
 }
 
 init().then(() => process.exit(0));
+
+async function configureBroker(rococoApi: ApiPromise, coretimeApi: ApiPromise): Promise<void> {
+  console.log(`Setting the initial configuration for the broker pallet`);
+
+  const configCall = u8aToHex(coretimeApi.tx.broker.configure(consts.CONFIG).method.toU8a());
+  const xcmCall = rococoApi.tx.xcmPallet.send(CORETIME_CHAIN, {V3: [
+    {
+      UnpaidExecution: {
+        check_origin: null,
+        weight_limit: "Unlimited"
+      }
+    },
+    {
+      Transact: {
+        originKind: "Superuser",
+        requireWeightAtMost: {
+          refTime: 1000000000,
+          proofSize: 900000,
+        },
+        call: {
+          encoded: configCall
+        }
+      }
+    }
+  ]});
+
+  const sudoCall = rococoApi.tx.sudo.sudo(xcmCall);
+
+  const alice = keyring.addFromUri("//Alice");
+
+  const callTx = async (resolve: () => void) => {
+      const unsub = await sudoCall.signAndSend(alice, (result: any) => {
+      if (result.status.isInBlock) {
+          unsub();
+          resolve();
+      }
+      });
+  };
+
+  return new Promise(callTx);
+}
 
 async function openHrmpChannel(rococoApi: ApiPromise, sender: number, recipient: number): Promise<void> {
     console.log(`Openeing HRMP channel between ${sender} - ${recipient}`);
@@ -42,9 +84,10 @@ async function openHrmpChannel(rococoApi: ApiPromise, sender: number, recipient:
 
     const alice = keyring.addFromUri("//Alice");
 
+    const openHrmp = rococoApi.tx.hrmp.forceOpenHrmpChannel(...newHrmpChannel);
+    const sudoCall = rococoApi.tx.sudo.sudo(openHrmp);
+
     const callTx = async (resolve: () => void) => {
-        const openHrmp = rococoApi.tx.hrmp.forceOpenHrmpChannel(...newHrmpChannel);
-        const sudoCall = rococoApi.tx.sudo.sudo(openHrmp);
         const unsub = await sudoCall.signAndSend(alice, (result: any) => {
         if (result.status.isInBlock) {
             unsub();
@@ -54,6 +97,17 @@ async function openHrmpChannel(rococoApi: ApiPromise, sender: number, recipient:
     };
 
   return new Promise(callTx);
+}
+
+const CORETIME_CHAIN = {
+  V3: {
+    parents: 0,
+    interior: {
+      X1: {
+        Parachain: CORETIME_CHAIN_PARA_ID
+      }
+    }
+  }
 }
 
 function featureFlag(flagName: string): boolean {
