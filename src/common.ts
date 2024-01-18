54f1c6a12e2a54e0b139501c20f6703753159506
Szegoo <sakacszergej@gmail.com>
2024-01-18 18:48:16 +0100

    use latest coretim-utils

--- src/common.ts
+++ src/common.ts
@@ -29,7 +29,10 @@ export async function transferRegion(
   log(`Transferring a reigon to ${receiver}`);
 
   const callTx = async (resolve: () => void) => {
-    const transfer = coretimeApi.tx.broker.transfer(regionId, receiver);
+    const transfer = coretimeApi.tx.broker.transfer(
+      { begin: regionId.begin, core: regionId.core, mask: regionId.mask.getMask() },
+      receiver
+    );
     const unsub = await transfer.signAndSend(sender, (result: any) => {
       if (result.status.isInBlock) {
         unsub();
